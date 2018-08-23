72db1d33a48ac6a9da3985906873a316db85cede
adigitalnative@gmail.com <adigitalnative@gmail.com>
2018-08-23 16:19:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!"
   }
 }
+
+function currentLine(line) {
+  if line.length === 0 {
+    return "The line is currently empty."
+  } else {
+    // do a thing
+    return "foo"
+  }
+}
