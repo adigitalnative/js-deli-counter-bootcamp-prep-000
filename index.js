7c1987f280aed85c00dfaacd1e0b1c9b0e918a7c
adigitalnative@gmail.com <adigitalnative@gmail.com>
2018-08-23 16:16:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(line, name) {
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    // return the first person in line and remove that person from the line
+    return katzDeliLine.shift();
   } else {
     return "There is nobody waiting to be served!"
   }
